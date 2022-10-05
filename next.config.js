75dfa2fbc82142864eaee73ce43adc161502af85
Elliot <elliot@fantastec.io>
2022-10-05 08:41:08 +0100

    linting

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-module.exports = {
-  reactStrictMode: true,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
+export const reactStrictMode = true;
+export const typescript = {
+  // !! WARN !!
+  // Dangerously allow production builds to successfully complete even if
+  // your project has type errors.
+  // !! WARN !!
+  ignoreBuildErrors: true,
 };
