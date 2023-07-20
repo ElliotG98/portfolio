501b4f82a10b7fffdaa4376eb5decd148c5a6d5d
Elliot Glaze <elliot.glaze@ITS1177.local>
2023-07-20 16:54:05 +0100

    Add image props

--- components/blog/image.tsx
+++ components/blog/image.tsx
@@ -1,6 +1,8 @@
 import NextImage from 'next/image';
+import { ImageProps } from 'next/image';
 
-// eslint-disable-next-line jsx-a11y/alt-text
-const Image = ({ ...rest }) => <NextImage {...rest} />;
+const Image = ({ src, width, height, alt, ...rest }: ImageProps) => (
+    <NextImage src={src} width={width} height={height} alt={alt} {...rest} />
+);
 
 export default Image;
